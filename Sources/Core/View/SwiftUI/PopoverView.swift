6322428253e9a62d3ab7aed53ef311aafb58385b
Michael Zimmermann <michael.zimmermann@adevinta.com>
2024-07-03 13:43:55 +0200

    [Popover] Migrate to new framework.

---
+++ Sources/Core/View/SwiftUI/PopoverView.swift
@@ -0,0 +1,25 @@
+//
+//  PopoverView.swift
+//  SparkPopover
+//
+//  Created by Michael Zimmermann on 02/07/2024.
+//  Copyright © 2024 Adevinta. All rights reserved.
+//
+
+import SwiftUI
+import SparkTheming
+@_spi(SI_SPI) import SparkCommon
+
+/// The SwiftUI version for the popover
+public struct PopoverView: View {
+
+    // MARK: - View
+
+    public var body: some View {
+        Text("Hello Popover")
+    }
+
+    // MARK: - Modifier
+
+    // TODO:
+}
